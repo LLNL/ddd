9a1c83b9331a2d95d36b4ef13c779c330ab06e38
Andrew Gillette <gillette7@llnl.gov>
2022-11-28 16:31:37 -0800

    Working on new functionality

--- run_static_trials.sh
+++ run_static_trials.sh
@@ -9,8 +9,8 @@ for ((j=1; j<$numtrials+1; j=j+1)) do
     echo
     # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/grwk_exp2.7/data_griewank_1000.csv 
     # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/grwk_exp3/data_griewank_100000.csv 
-    python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_gauss/parab_exp3/data_paraboloid_100000.csv --validsplit 0.1
-    # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_gauss/grwk_exp3/data_griewank_100000.csv 
+    python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/parab_exp3/data_paraboloid_10000.csv
+    # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/grwk_exp3/data_griewank_1000.csv 
 done
 
 ls zz-temp-seed*.csv > allfiles.multi
