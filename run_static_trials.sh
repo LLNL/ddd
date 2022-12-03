d7ae655edf9c79f4b228d69f6e0df15154a138e0
Andrew Gillette <gillette7@llnl.gov>
2022-12-02 16:28:44 -0800

    Working on new functionality

--- run_static_trials.sh
+++ run_static_trials.sh
@@ -3,14 +3,14 @@
 #############################################################################################
 
 jobid="123456"
-numtrials="9"
+numtrials="50"
 
 for ((j=1; j<$numtrials+1; j=j+1)) do
     echo
     # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/grwk_exp2.7/data_griewank_1000.csv 
     # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/grwk_exp3/data_griewank_100000.csv 
-    python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/parab_exp3/data_paraboloid_10000.csv
-    # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/grwk_exp3/data_griewank_1000.csv 
+    # python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/parab_exp3/data_paraboloid_100000.csv
+    python delaunay_density_diagnostic.py --jobid ${jobid} --seed ${j} --staticdata staticdata/from_ball/grwk_exp3/data_griewank_100000.csv 
 done
 
 ls zz-temp-seed*.csv > allfiles.multi
