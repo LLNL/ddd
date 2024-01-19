f1195b874a61c81145519e60dc5e657bd627f6bb
Andrew Gillette <gillette7@llnl.gov>
2024-01-19 15:30:34 -0800

    Removing csv mgmt file

--- remove_csvs.py
+++
@@ -1,7 +0,0 @@
-#############################################################################################
-# Removes all *csv files and allfiles.multi from ddd root directory
-#############################################################################################
-
-import subprocess
-subprocess.call('rm *csv', shell=True)
-subprocess.call('rm allfiles.multi', shell=True)
